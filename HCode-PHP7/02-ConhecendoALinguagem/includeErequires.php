b3aa20c19119a51b588bc06a07cab4fd054605a2
Savio Nascimento <savionascimentodev@gmail.com>
2023-02-14 17:48:50 -0400

    ✨ added some class module 2

---
+++ HCode-PHP7/02-ConhecendoALinguagem/includeErequires.php
@@ -0,0 +1,12 @@
+<?php
+
+// Exportando a função somar com o método require;
+require 'somar.php';
+
+// Exportando a função com o método include;
+// include 'somar.php';
+
+$resultado = somar(10,20);
+
+echo $resultado
+?>
---
+++ HCode-PHP7/02-ConhecendoALinguagem/somar.php
@@ -0,0 +1,6 @@
+<?php
+// Função feita para ser chamada em outro arquivo para testar includes e requires;
+function somar($a, $b) {
+  return $a + $b;
+}
+?>
